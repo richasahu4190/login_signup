ebf7e796ddfab09249d0b238c0e78de33acd0144
richasahu4190 <118303012+richasahu4190@users.noreply.github.com>
2024-04-07 20:57:14 +0530

    Update index.js

--- services/index.js
+++ services/index.js
@@ -2,7 +2,7 @@
 
 export const register_user = async (formData) => {
     try {
-        const res = await fetch('http://localhost:3000/api/Auth/register', {
+        const res = await fetch('http://login-signup-seven-alpha.vercel.app/api/Auth/register', {
             headers: {
                 'Content-Type': 'application/json',
             },
@@ -20,7 +20,7 @@ export const register_user = async (formData) => {
 
 export const login_user = async (formData) => {
     try {
-        const res = await fetch('http://localhost:3000/api/Auth/login', {
+        const res = await fetch('https://login-signup-seven-alpha.vercel.app/api/Auth/login', {
             headers: {
                 'Content-Type': 'application/json',
             },
