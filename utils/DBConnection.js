795e4eda87803c6b9d41601b4f97316e25feb782
richasahu4190 <118303012+richasahu4190@users.noreply.github.com>
2024-04-07 14:27:39 +0530

    Update DBConnection.js

--- utils/DBConnection.js
+++ utils/DBConnection.js
@@ -1,10 +1,6 @@
 import {Pool} from 'pg';
 const pool=new Pool({
-    user: "postgres",
-    host: "localhost",
-    database:"mydb",
-    password :"1234567890",
-    port: 5432,
+    connectionString: process.env.POSTGRES_URL ,
 });
 
 export default pool;
