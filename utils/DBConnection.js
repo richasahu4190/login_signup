7940a1709c10ed2fe2912683cd77c3cf394f1ebe
richasahu4190 <118303012+richasahu4190@users.noreply.github.com>
2024-04-07 20:52:11 +0530

    Update DBConnection.js

--- utils/DBConnection.js
+++ utils/DBConnection.js
@@ -1,6 +1,13 @@
-import {Pool} from 'pg';
-const pool=new Pool({
-    connectionString: process.env.POSTGRES_URL ,
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    user: 'mydb_owner',
+    password: 'JnCkMP16GgRQ',
+    host: 'ep-long-unit-a5wtgnce.us-east-2.aws.neon.tech',
+    database: 'mydb',
+    ssl: {
+        sslmode: 'require'
+    }
 });
 
 export default pool;
